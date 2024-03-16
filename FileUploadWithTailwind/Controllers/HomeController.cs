a6fafb92127e2bed02df7f68887c3309f16976c3
Kaushik Tadhani <tadkaushik@gmail.com>
2024-03-16 15:35:20 -0400

    file save fixdd

--- FileUploadWithTailwind/Controllers/HomeController.cs
+++ FileUploadWithTailwind/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using FileUploadWithTailwind.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace FileUploadWithTailwind.Controllers
 {
@@ -16,6 +15,27 @@ namespace FileUploadWithTailwind.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveImage([FromBody] SaveImageRequestModel request)
+        {
+            // Convert the data URL to a byte array
+            var imageBytes = Convert.FromBase64String(request.ImageDataUrl.Split(',')[1]);
+
+            // Extract the image format from the data URL
+            string format = request.ImageDataUrl.Split(';')[0].Split('/')[1];
+
+            string fileName = string.Format("{0}.{1}", DateTime.Now.ToString("MM-dd-yyyy-HH-mm-ss"), format);
+
+            // Save the image bytes to a file with the specified path
+            var filePath = request.FilePath + fileName;
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                stream.Write(imageBytes, 0, imageBytes.Length);
+            }
+
+            return Ok();
         }
     }
 }
---
+++ FileUploadWithTailwind/Models/SaveImageRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FileUploadWithTailwind.Models
+{
+    public class SaveImageRequestModel
+    {
+        public string ImageDataUrl { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
