9f1e1934c21bb8db6aaccc3a3054da0a9618308c
Kaushik Tadhani <tadkaushik@gmail.com>
2024-03-16 15:35:19 -0400

    Add project files.

---
+++ FileUploadWithTailwind/Models/PhotoUploadViewModal.cs
@@ -0,0 +1,10 @@
+﻿namespace FileUploadWithTailwind.Models
+{
+    public class PhotoUploadViewModal
+    {
+        public required string PopupTitle { get; set; }
+        public required string ContextId { get; set; }
+        public required string DestinationFolder { get; set; }
+        public required string JavaScriptCallBackFunction { get; set; }
+    }
+}
---
+++ FileUploadWithTailwind/Views/Shared/Components/PhotoUpload/PhotoUploadViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using FileUploadWithTailwind.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadWithTailwind.Views.Shared.Components.PhotoUpload
+{
+    public class PhotoUploadViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(PhotoUploadViewModal parameters)
+        {
+            return View(parameters);
+        }
+    }
+}
